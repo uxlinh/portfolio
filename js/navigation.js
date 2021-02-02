28b448c87cc5bb73e34b467d0f05a7f3a954aa0d
uxlinh <linh.tn94@gmail.com>
2021-02-02 13:23:24 +0100

    added animation to burgermenu

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,15 @@
 let mainNav = document.getElementById("js-menu");
-
-
+let bar = document.getElementById("bar");
 let navBarToggle = document.getElementById("js-navbar-toggle");
+//const burger = document.querySelector('.burgerlines');
 
-navBarToggle.addEventListener("click", function() {
-  mainNav.classList.toggle("active");
+navBarToggle.addEventListener("click", event => {
+    let activeB = mainNav.classList.toggle("active");
+    navBarToggle.classList.toggle('toggle'); 
+     
+    if (activeB) {
+    bar.style.backgroundColor = "#8351ED"; 
+    } else {
+    bar.style.backgroundColor = "transparent";
+    }
 });
