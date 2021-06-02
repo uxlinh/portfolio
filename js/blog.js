ca3096da2ece686c3b50aabcbad3a187f46d4fdc
uxlinh <linh.tn94@gmail.com>
2021-06-02 13:28:04 +0200

    thumbnailbilder og blogginnleggende funker

--- js/blog.js
+++ js/blog.js
@@ -4,33 +4,35 @@
 let blogContainer = document.querySelector('#blog_container');
 
 app.content.get({
-	schemaKey: 'blog', // navnet på schema-et ditt
-	populate: [{
-		field: 'mainImage',
-		size: {
-			height: 9999,
-			quality: 1,
-			width: 375
+		schemaKey: 'blog', // navnet på schema-et ditt
+		populate: [{
+			field: 'mainImage',
+			size: {
+				height: 9999,
+				quality: 1,
+				width: 375
+			}
+		}]
+	})
+
+	.then(blog => {
+		let html = "";
+
+		for (const property in blog) {
+			console.log('all of your projects', blog);
+			let blogPostThumbnail = blog[property];
+
+			html += `
+			<a class="blog-listing" href="./blogpost.html?id=${blogPostThumbnail.id}">
+				<img src="${blogPostThumbnail.mainImage[0].url}" class="imgThumbnail">
+				<h2>${blogPostThumbnail.blogTitle}</h2>
+				<p>${ blogPostThumbnail.textArea }</p>
+			</a>
+			`;
 		}
-	}]
-})
 
-.then(blog => {
-	let html = "";
-
-	for(const property in blog) {
-        console.log('all of your projects', blog);
-		let proj = blog[property];
-
-		html += `
-		<a class="blog-listing" href="./blogpost.html?id=${proj.id}">
-			<h2>${proj.blogTitle}</h2>
-			<p>${ proj.textArea }</p>
-			<img src="${proj.mainImage[0].url}" class="img-thumbnail">
-		</a>
-		`;
-	}
-
-	blogContainer.innerHTML = html;
-})
-.catch(error => {console.error('Det skjedde en feil', error);})
+		blogContainer.innerHTML = html;
+	})
+	.catch(error => {
+		console.error('Det skjedde en feil', error);
+	})
